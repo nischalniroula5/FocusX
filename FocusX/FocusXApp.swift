bdcb9ce12c2ab14a1ba6d714985b0cf9f9c23fb9
Nischal Niroula <officialnischalniroula@gmail.com>
2023-07-24 22:24:43 +1000

    Initial Commit

---
+++ FocusX/FocusXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusXApp.swift
+//  FocusX
+//
+//  Created by Nischal Niroula on 24/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct FocusXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
